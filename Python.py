d4d702ec25a41ea7383872ac356abd817d013bdf
Yidale <56829726+Yidale@users.noreply.github.com>
2019-10-21 23:14:41 +0800

    修改文件
    修改文件添加Excel操作

--- Python.py
+++ Python.py
@@ -1,1 +1,11 @@
-import 
+#coding:utf-8
+import xlrd
+# 确定Excel位置并打开
+Excelpath ='../Testdata/excel_case.xlsx'
+data = xlrd.open_workbook(Excelpath)
+# 选择Excel第一页sheet
+tables = data.sheets()[0]
+# 打印Excel行数
+print(tables.nrows)
+# 打印Excel第二行，第三列内容
+print(tables.cell_value(1,2))
